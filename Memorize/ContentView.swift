55381eed5b08efe8c5d5545d3d272c83e0e84d32
Arunsakh <arunsakh@gmail.com>
2023-09-23 05:25:51 +0700

    Created CardView

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,15 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        //@ViewBuilder
+        // HStack<TupleView<CardView, CardView, CardView, CardView>>
+        HStack{
+            CardView(symbol: "👻", isFaceUp: true)
+            CardView(symbol: "👻")
+            CardView(symbol: "👻")
+            CardView(symbol: "👻")
+            
         }
+        
+        .foregroundColor(.orange)
         .padding()
     }
 }
+
+
+struct CardView: View {
+    
+    var symbol: String
+    var isFaceUp = false
+    
+    var body: some View {
+        if isFaceUp {
+            ZStack {
+                RoundedRectangle(cornerRadius: 12).foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 2)
+                Text(symbol).font(.largeTitle)
+            }
+        } else {
+            RoundedRectangle(cornerRadius: 12)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
